20b05a83118800884d630e12655fdb163139eb21
Artur <lhfrjyxbr21@gmail.com>
2020-01-03 14:34:28 +0200

    Вывод максимального числа из 3 заданных

---
+++ 1.js
@@ -0,0 +1,11 @@
+var number1=412//variable initialization
+var number2=325  
+var number3 =12351523 
+ 
+var Max = number1//equality test
+if (number2 > Max) Max = number2
+if (number3 > Max) Max = number3
+ 
+console.log(Max)
+
+
