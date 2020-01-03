a944e67c5bdb562454101a61b70c1cf5bc7a5332
vanLesson <55431955+vanLesson@users.noreply.github.com>
2020-01-03 16:24:19 +0200

    Проверка на пригодность в армию

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
 var age = 21 //variable initialization
 if (age >= 20 && age <= 27) {//equality test
-    console.info("Выслать повестку!")// console output
+    console.info("Выслать повестку!")// console output 
 }
 else console.info("Откосил")
