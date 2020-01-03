04faf1123ffde0f049d25bdc0a40f0d394476066
vanLesson <55431955+vanLesson@users.noreply.github.com>
2020-01-03 16:24:40 +0200

    проверка на выходные

--- 3.js
+++ 3.js
@@ -1,3 +1,3 @@
 var day = new Date//output to the console Get the day number in week
 var weaks_day = day.getDay()
-console.log((weaks_day === 0 || weaks_day === 6) ? console.log("Выходные") : console.log("Пора на работу"))
+console.log((weaks_day === 0 || weaks_day === 6) ? console.log("Выходные") : console.log("Пора на работу")) 
