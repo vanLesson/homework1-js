34896187111aeff901c0da2391e79b5f4e9e4204
vanLesson <55431955+vanLesson@users.noreply.github.com>
2020-01-03 16:23:50 +0200

    Поиск найбольшего числа из 3

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@ var number3 = 12351523
 
 var Max = number1//equality test
 if (number2 > Max) Max = number2
-if (number3 > Max) Max = number3
+if (number3 > Max) Max = number3 
 
 console.log(Max)
 
